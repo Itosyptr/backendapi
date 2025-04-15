75dbc02e562a563e0b669d60806b5bb3c7993169
Muhammad Trisucipto <itokeren16@gmail.com>
2025-04-15 18:34:36 +0700

    Feat: Update route

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/auth', authRoutes);
 
+app.get("/", (req, res) => {
+    res.send({
+        message: "Api Wasteapp",
+        author: "https://github.com/wasteapptech/wasteapp_flutterapp",
+    });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server running on port https://localhost:${port}`);
