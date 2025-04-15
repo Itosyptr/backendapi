cc7d9ef21d15b6a29e7e74b1d8ee7506c2d147b2
Muhammad Trisucipto <itokeren16@gmail.com>
2025-04-15 18:53:06 +0700

    Feat: Udate logic

--- index.js
+++ index.js
@@ -3,18 +3,30 @@ const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const app = express();
 
-
 app.use(bodyParser.json());
 app.use('/auth', authRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send({
+        message: "Kemana man??",
+    });
+});
+
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.get("/", (req, res) => {
     res.send({
-        message: "Api Wasteapp",
-        author: "https://github.com/wasteapptech/wasteapp_flutterapp",
+        message: "Api Smartdorm",
+        author: "Smartdorm",
     });
 });
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Server running on port https://localhost:${port}`);
+  console.log(`Server running on port http://localhost:${port}`);
 });
