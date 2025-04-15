104f854b017f239821b996b867bb8516da107ea6
Muhammad Trisucipto <itokeren16@gmail.com>
2025-04-15 19:03:24 +0700

    Feat: Fix bug

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-
+app.use(express.json());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -13,19 +13,20 @@ app.use((req, res, next) => {
 });
 app.use('/auth', authRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).send({
-        message: "Kemana man??",
-    });
-});
-
-
 app.get("/", (req, res) => {
     res.send({
         message: "Api Smartdorm",
         author: "Smartdorm",
     });
 });
+
+
+app.use((req, res, next) => {
+    res.status(404).send({
+        message: "Kemana man??",
+    });
+});
+
 
 const port = 3000;
 app.listen(port, () => {
